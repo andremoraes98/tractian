59ec9bf7ac0ff8c3ca186ced98958407153969fe
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-29 09:49:11 -0300

    feat: renderização do Header e da página Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Main from './pages/Main';
 
 function App() {
   return (
-    <h1>Lets go</h1>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={ <Main /> }/>
+      </Routes>
+    </>
   );
 }
 
