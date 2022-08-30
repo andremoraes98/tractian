7cde83ac387a8c65b711627f4f6b5561653bdd58
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-30 11:57:47 -0300

    feat: adição da rota create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
+import CreateForms from './pages/CreateForms/CreateForms';
 import Industry from './pages/Industry';
 import Main from './pages/Main';
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <SideBar />
       <Routes>
+        <Route path="/create" element={ <CreateForms /> }/>
         <Route path="/:industry" element={ <Industry /> }/>
         <Route path="/" element={ <Main /> }/>
       </Routes>
