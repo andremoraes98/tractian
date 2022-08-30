01c1a5e3b6a557eba974a49e32b7017ad1a958e5
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-29 22:08:45 -0300

    feat: renderização condicional do sidebar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import LOGO from '../../images/LOGO.png';
-import { AiOutlineMenu } from 'react-icons/ai'
+import { RiMenuLine, RiMenuUnfoldFill } from 'react-icons/ri'
 import './Header.css'
 import { Link } from 'react-router-dom';
+import TractianContext from '../../context/TractianContext';
 
 function Header() {
+  const { showSideBar, setShowSideBar } = useContext(TractianContext);
   return (
     <header className='display-flex'>
       <Link to="/">
         <img src={ LOGO }/>
       </Link>
-      <AiOutlineMenu />
+      {
+        showSideBar
+          ? <RiMenuUnfoldFill
+            onClick={ () => setShowSideBar(!showSideBar) }
+          />
+          : <RiMenuLine
+            onClick={ () => setShowSideBar(!showSideBar) }
+          />
+      }
     </header>
   );
 }
