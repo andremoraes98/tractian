916104a6192831b19d84f85b0af57412d72e67ef
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-31 11:00:01 -0300

    feat: criação do estado dos ativos e do loading

--- src/context/TractianContextProvider.js
+++ src/context/TractianContextProvider.js
@@ -8,11 +8,81 @@ function TractianContextProvider({ children }) {
   const [assetInfo, setAssetInfo] = useState({});
   const [showConfirmMessage, setShowConfirmMessage] = useState(false);
   const [confirmMessage, setConfirmMessage] = useState('');
+  const [assets, setAssets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
 
   const toggleConfirmMessage = (message, willConfirmMessageShow) => {
     setShowConfirmMessage(willConfirmMessageShow);
     setConfirmMessage(message);
-  }
+  };
+
+  const getAssets = () => {
+    setIsLoading(true);
+    setAssets([
+      {
+        id: 1,
+        name: 'TM103',
+        model: 'Siemens',
+        owner: 'Industria Freios Supremos 1',
+        status: 'Running',
+        helthLevel: '90',
+        image: 'https://assets.new.siemens.com/siemens/assets/api/uuid:4eeebb1d-1ae3-4103-99e2-d8d63a1776e6/width:1125/quality:high/simotics-gp-vsd-10-line.png',
+        energy: [],
+        temp: [],
+        vibration: [],
+      },
+      {
+        id: 2,
+        name: 'SL203',
+        model: 'Siemens',
+        owner: 'Industria Freios Supremos 1',
+        status: 'Running',
+        helthLevel: '90',
+        image: 'https://assets.new.siemens.com/siemens/assets/api/uuid:4eeebb1d-1ae3-4103-99e2-d8d63a1776e6/width:1125/quality:high/simotics-gp-vsd-10-line.png',
+        energy: [],
+        temp: [],
+        vibration: [],
+      },
+      {
+        id: 3,
+        name: 'HT107',
+        model: 'Siemens',
+        owner: 'Industria Freios Supremos 1',
+        status: 'Running',
+        helthLevel: '90',
+        image: 'https://assets.new.siemens.com/siemens/assets/api/uuid:4eeebb1d-1ae3-4103-99e2-d8d63a1776e6/width:1125/quality:high/simotics-gp-vsd-10-line.png',
+        energy: [],
+        temp: [],
+        vibration: [],
+      },
+      {
+        id: 4,
+        name: 'MU503',
+        model: 'Siemens',
+        owner: 'Industria Freios Supremos 1',
+        status: 'Running',
+        helthLevel: '90',
+        image: 'https://assets.new.siemens.com/siemens/assets/api/uuid:4eeebb1d-1ae3-4103-99e2-d8d63a1776e6/width:1125/quality:high/simotics-gp-vsd-10-line.png',
+        energy: [],
+        temp: [],
+        vibration: [],
+      },
+      {
+        id: 5,
+        name: 'TM502',
+        model: 'Siemens',
+        owner: 'Industria Freios Supremos 1',
+        status: 'Running',
+        helthLevel: '90',
+        image: 'https://assets.new.siemens.com/siemens/assets/api/uuid:4eeebb1d-1ae3-4103-99e2-d8d63a1776e6/width:1125/quality:high/simotics-gp-vsd-10-line.png',
+        energy: [],
+        temp: [],
+        vibration: [],
+      },
+    ]);
+    setIsLoading(false);
+  };
 
   const context = {
     industryName,
@@ -24,6 +94,10 @@ function TractianContextProvider({ children }) {
     showConfirmMessage,
     confirmMessage,
     toggleConfirmMessage,
+    assets,
+    getAssets,
+    isLoading,
+    setIsLoading,
   };
 
   return(
