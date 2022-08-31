b4c3ca8a3335c98263385ff3735f4473eb5409ce
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-31 12:01:38 -0300

    feat: implementação do redirecionamento para o /delete

--- src/components/CRUD/Delete.js
+++ src/components/CRUD/Delete.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { AiFillDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 function Delete() {
+  const navigate = useNavigate();
+
   return (
     <div
       id="unit-delete"
       className="unit unit-flex"
+      onClick={ () => navigate('/delete') }
     >
       <h1 className="unit-title">Deletar</h1>
       <AiFillDelete className="simbol-unit" />
