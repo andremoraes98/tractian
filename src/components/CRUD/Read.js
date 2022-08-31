82c32910cdba171dcbe59303fc3fa0a39fb8a57e
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-30 22:43:00 -0300

    feat: implementação do redirecionamento para o /read

--- src/components/CRUD/Read.js
+++ src/components/CRUD/Read.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BsFillGearFill } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 function Read() {
+  const navigate = useNavigate();
+
   return (
     <div
       id="unit-read"
       className="unit unit-flex"
+      onClick={ () => navigate('/read') }
     >
       <h1 className="unit-title">Monitorar</h1>
       <BsFillGearFill className="simbol-unit" />
