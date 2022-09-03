df1c81d04c78f2406a230e28be6a4a542eab6ebd
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-03 13:11:13 -0300

    feat: implementação do Loading e ajuste para key e id do Aset

--- src/pages/DeletePage/DeletePage.js
+++ src/pages/DeletePage/DeletePage.js
@@ -1,35 +1,41 @@
 import React, { useContext, useEffect } from 'react';
 import Aset from '../../components/Aset/Aset';
 import DeleteMessage from '../../components/DeleteMessage/DeleteMessage';
+import Loading from '../../components/Loading/Loading';
 import TractianContext from '../../context/TractianContext';
 import './DeletePage.css'
 
 function DeletePage() {
-  const { assets, getAssets } = useContext(TractianContext);
+  const { assets, getAssets, isLoading } = useContext(TractianContext);
 
   useEffect(() => {
     getAssets();
   }, []);
 
   return (
-    <main id="update-assets">
-      <h1 className="title red">Selecione o ativo que deseja <strong>Excluir</strong>:</h1>
-      <div
-        id="assets"
-        className="display-flex space-around"
-      >
-        {
-          assets.map((aset) => (
-            <Aset
-              key={ aset.id }
-              aset={ aset }
-              color="red"
-            />
-          ))
-        }
-      </div>
-      <DeleteMessage />
-    </main>
+    isLoading
+      ? (<Loading />)
+      : (
+        <main id="update-assets">
+          <h1 className="title red">Selecione o ativo que deseja <strong>Excluir</strong>:</h1>
+          <div
+            id="assets"
+            className="display-flex space-around"
+          >
+            {
+              assets.map((aset, index) => (
+                <Aset
+                  key={ index + 1 }
+                  aset={ aset }
+                  color="red"
+                  id={ index + 1}
+                />
+              ))
+            }
+          </div>
+          <DeleteMessage />
+        </main>
+      )
   )
 }
 
