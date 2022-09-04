45a99febc3f45791dc159ee1ac48fbfb0fc638f8
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-04 20:15:05 -0300

    feat: ajuste na renderização do componente Unit

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Unit from '../../components/Unit/Unit';
+import TractianContext from '../../context/TractianContext';
+import Loading from '../../components/Loading/Loading'
 import './Main.css'
 
 function Main() {
+  const {
+    users,
+    getUsers,
+    isLoading,
+  } = useContext(TractianContext);
+  
+  useEffect(() => {
+    getUsers()
+  }, []);
+
   return (
-    <section className="display-flex">
-      <h1 className="title">Selecione a unidade que deseja monitorar:</h1>
-      <Unit unitName="Industria Freios Supremos 1" />
-      <Unit unitName="Industria Freios Supremos 2" />
-    </section>
+    isLoading
+    ? ( <Loading /> )
+    : (
+      <section className="display-flex">
+        <h1 className="title">Bem vindo à Industria Freios Supremos!</h1>
+        <h3 className="subtitle">Escolha o seu usuário:</h3>
+        {
+          users.map((user) => <Unit key={ user.user } credentials={ user } />)
+        }
+      </section>
+    )
   );
 }
 
